a69e3f6e864878df18b97cb46496c526b32a8dcc
Nitin Chandani <nitin1chandani@gmail.com>
2023-06-21 01:48:51 +0530

    more changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { IMG_CDN_URL } from "../constants.js";
 const Card = ({ name, cuisines, cloudinaryImageId, lastMileTravelString }) => {
   return (
     <div className="card">
-      <img src={IMG_CDN_URL + cloudinaryImageId} />
+      <img src={IMG_CDN_URL + cloudinaryImageId} alt="restraunt" />
       <h2>{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
       <h4>{lastMileTravelString} minutes</h4>
